a4ed53860040cc61e98c365e1b3b0822d21fc3fe
Mateusz Nguyen Tien <mateusznguyentien@gmail.com>
2019-04-25 21:11:16 +0200

    init

---
+++ SlotGame/src/test/java/pl/mateusznguyentien/slotgame/SlotGameApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mateusznguyentien.slotgame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlotGameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
